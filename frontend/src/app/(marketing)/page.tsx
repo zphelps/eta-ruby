3483af4f8ef8cc7004e66fd7f5560232e1c01c41
David Wolfe Bender <davidwolfebender@gmail.com>
2024-09-04 18:20:13 -0400

    initial marketing commit

--- frontend/src/app/(marketing)/page.tsx
+++ frontend/src/app/(marketing)/page.tsx
@@ -1,18 +1,24 @@
 "use client";
 import {redirect} from "next/navigation";
+import Header from "@/components/marketing/header";
+import Hero from "@/components/marketing/hero";
+import Features from "@/components/marketing/features";
+import Testimonials from "@/components/marketing/testimonials";
+import Pricing from "@/components/marketing/pricing";
+import Footer from "@/components/marketing/footer";
 
 export default function Home() {
-    redirect('/editor');
+    // redirect('/editor');
 
-    // return (
-    //   <section className={'container'}>
-    //       <Header/>
-    //       <Hero/>
-    //       <Features/>
-    //       <Pricing/>
-    //       <Testimonials/>
-    //       {/*<FAQ/>*/}
-    //       <Footer/>
-    //   </section>
-    // );
+    return (
+      <section className={'container'}>
+          <Header/>
+          <Hero/>
+          <Features/>
+          <Pricing/>
+          <Testimonials/>
+          {/*<FAQ/>*/}
+          <Footer/>
+      </section>
+    );
 }
